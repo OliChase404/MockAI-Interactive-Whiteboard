534cd567dd37e54f4813a721ac119f873356c0e6
Oli Chase <olichase91@gmail.com>
2024-04-13 17:37:32 -0700

    Websockets Basic Setup

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
 # app.py
 from flask import Flask
+from flask_socketio import SocketIO
+from flask_cors import CORS
+
 
 app = Flask(__name__)
+socket = SocketIO(app, cors_allowed_origins="*")
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+@socket.on('connect')
+def handle_connect():
+    print('Client connected')
+    
+@socket.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected')
+    
+@socket.on('message')
+def handle_message(message):
+    print('Received message:', message)
+    socket.send(message)
+    
+@socket.on('drawing')
+def handle_drawing(data):
+    print('Received drawing:', data)
+    socket.emit('drawing', data, broadcast=True)
 
 @app.route('/')
 def index():
     return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socket.run(app, debug=True)
--- client/whiteboard/src/app/page.tsx
+++ client/whiteboard/src/app/page.tsx
@@ -2,6 +2,25 @@
 import styles from "./page.module.css";
 import { Tldraw } from "tldraw";
 import './index.css'
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:5000')
+
+socket.on('connect', () => {
+  console.log('connected to server')
+})
+
+socket.on('disconnect', () => {
+  console.log('disconnected from server')
+})
+
+socket.on('drawing', (data) => {
+  console.log('Received drawing: ', data)
+})
+
+function handleNewDrawing(data: any) {
+  socket.emit('drawing', data)
+}
 
 function Canvas() {
 
